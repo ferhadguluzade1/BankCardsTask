184ea144e6bd649f1bca0ca3cacea0c079724bf4
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-10-24 12:58:52 +0400

    Program.cs

--- Program.cs
+++ Program.cs
@@ -9,11 +9,12 @@ public class Card
     private double cashback;
 
 
-    public Card(string ownerName)
+    public Card(string ownerName, double balance)
     {
         Console.WriteLine(Balance);
         Console.WriteLine(cashback);
         OwnerName = ownerName;
+        //Console.WriteLine(ownerName);
         
     }
 
@@ -24,6 +25,7 @@ public class Card
 
     public virtual void Withdraw(double amount)
     {
+
         if (Balance < amount)
         {
             Console.WriteLine("Insufficient balance");
@@ -35,20 +37,20 @@ public class Card
     }
     public static void Main()
     {
-        Card userCard = new Card("Ferhad Guluzade");
+        Card userCard = new Card("Ferhad Guluzade", 3000);
         userCard.CardNumber = 2828290102827896;
         userCard.OwnerName = "Ferhad Guluzade";
         userCard.AnnualFee=100;
         Console.WriteLine("Balance: "+ userCard.Balance);
 
 
-        PremiumCard premiumCard = new PremiumCard();
+        PremiumCard premiumCard = new PremiumCard("Ferhad Guluzade", 3000);
    
 
-        BuisinessCard buisinessCard = new BuisinessCard();
+        BuisinessCard buisinessCard = new BuisinessCard("Ferhad Guluzade", 3000);
 
 
-        StandartCard standartCard = new StandartCard();
+        StandartCard standartCard = new StandartCard("Ferhad Guluzade", 3000);
     }
 
 
