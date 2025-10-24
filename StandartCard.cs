f59b59d9ef4ebd13a22e03a349cf29698449ac91
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-10-24 12:59:14 +0400

    StandartCard.cs

--- StandartCard.cs
+++ StandartCard.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,17 @@ namespace HomeTaskFor24._10._25._14oclock
     internal class StandartCard :Card
     {
         private double cashback;
+        public StandartCard(string ownerName, double balance) :base(ownerName, balance)
+        {
+            
+        }
 
         public override void Withdraw(double amount)
         {
             cashback = amount * 1 / 100;
             amount += cashback;
-            Console.WriteLine(amount);
+            Console.WriteLine("Cashback: " + cashback);
+            Console.WriteLine("Amount: " + amount);
         }
         
 
