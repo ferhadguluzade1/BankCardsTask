cc97427673ef49ebaa9816852c1d4dfffe3953b8
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-10-24 05:07:45 +0400

    PremiumCard.cs

---
+++ PremiumCard.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskFor24._10._25._14oclock
+{
+    internal class PremiumCard : Card
+    {
+        private double cashback;
+        public override void Withdraw(double amount)
+        {
+            while (amount <= 5000)
+            {
+                cashback = amount * 3 / 100;
+                amount += cashback;
+                Console.WriteLine(amount);
+            }
+            
+        }
+
+
+
+    }
+}
