4b3057345c7bc07d628b124088e1bf5223d22070
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-10-24 12:58:23 +0400

    PremiumCard.cs

--- PremiumCard.cs
+++ PremiumCard.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 
 namespace HomeTaskFor24._10._25._14oclock
 {
-    internal class PremiumCard : Card
+    internal class PremiumCard: Card
     {
         private double cashback;
+        public PremiumCard(string ownerName, double balance) : base(ownerName, balance)
+        {
+
+        }
         public override void Withdraw(double amount)
         {
             while (amount <= 5000)
             {
                 cashback = amount * 3 / 100;
                 amount += cashback;
-                Console.WriteLine(amount);
+                Console.WriteLine("Cashback: " + cashback);
+                Console.WriteLine("Amount: "+ amount);
             }
             
         }
